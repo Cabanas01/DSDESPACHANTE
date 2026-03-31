fbd8b006fcf4a6538dad995a0784dabbd26c2aa9
DESPACHANTE DS ASSESSORIA TRÂNSITO <dsassessoriaservicosdetransito@gmail.com>
2026-03-31 05:53:28 +0000

    +1.284 formados

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -62,7 +62,7 @@ export function Hero() {
                     <Star key={i} className="w-3 h-3 fill-secondary text-secondary" />
                   ))}
                 </div>
-                <span className="text-xs font-bold text-white">1.284 Alunos Formados</span>
+                <span className="text-xs font-bold text-white">+1.284 formados</span>
               </div>
             </div>
           </div>
